f7d76d8a2924ab2fec8339826e8fecb2b2163adf
Daltimore <obinnaleo78@gmail.com>
2023-01-11 23:22:01 +0100

    frontend setup

---
+++ apps/medix-frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div class="flex justify-center items-center m-auto">
+      <h3 class="text-3xl">Medix Frontend</h3>
+    </div>
+  </main>
+</template>
